b4754338a9b71f908580c73df1038ef30aeb7383
Alexis Gonzalez <87156515+Alex-is-Gonzalez@users.noreply.github.com>
2024-05-20 18:37:51 -0700

    Create objectsMethods.js

---
+++ objectsMethods.js
@@ -0,0 +1,50 @@
+/*
+1. Adding Properties
+
+Q: How do you add a property name with the value John to an object? Ex. Add your name to an empty object
+*/
+
+
+/*
+2. Checking Property Existence
+
+Q: How do you check if the age property exists in the following object?
+let person = { name: 'Alice' };
+ 
+*/
+
+
+
+
+
+/*
+3. Iterating Over Properties
+
+Q: How do you iterate over all properties of the following object and log each key and value?
+let person = { name: 'Alice', age: 25 };
+
+*/
+
+
+ /* 
+4. Object keys -> Array
+Q: How do you get an array of all the keys in the following object?
+let person = { name: 'Alice', age: 25 };
+
+ */ 
+
+
+
+/* 
+5. Nested Objects
+
+Q: How do you access the city property of the following nested object?
+let person = { 
+  name: 'Alice', 
+  address: { 
+    city: 'Wonderland', 
+    zip: '12345' 
+  }
+};
+
+*/
